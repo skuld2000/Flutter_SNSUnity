bc6263df73eb746d71774239b27c86beb6b82294
SangWoo <semozz@AL02027333.local>
2023-09-11 17:57:44 +0900

    List.generator/CircleAvatar

--- lib/widgets/home_widget.dart
+++ lib/widgets/home_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class HomeWidget extends StatelessWidget {
@@ -7,55 +9,82 @@ class HomeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: SingleChildScrollView(
-          child: Column(
-        children: [
-          for (int i = 0; i < 5; ++i) PostCard(),
-        ],
-      )),
+        child: Column(
+          children: List.generate(100, (index) {
+            return PostCard();
+          }),
+          //for (int i = 0; i < 5; ++i) PostCard(),
+        ),
+      ),
     );
   }
 
-  Card PostCard() {
-    return Card(
-      color: Colors.white,
-      //surfaceTintColor: Colors.blue,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              CircleAvatar(),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text("UserName"),
-                  Text("2023/09/11"),
-                ],
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Image.network(
-              "https://i.pinimg.com/564x/8c/da/8c/8cda8ca891afa64893369e592fd94822.jpg"),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: [
-              Icon(Icons.favorite),
-              Text("100"),
-              SizedBox(
-                width: 10,
-              ),
-              Icon(Icons.chat),
-              Text("99"),
-            ],
-          )
-        ],
+  Widget PostCard() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        color: Colors.white,
+        //surfaceTintColor: Colors.blue,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.transparent,
+                  backgroundImage: NetworkImage(
+                      "https://i.pinimg.com/564x/8c/da/8c/8cda8ca891afa64893369e592fd94822.jpg"),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("UserName"),
+                    Text("2023/09/11"),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Image.network(
+              //width: 300,
+              //height: 300,
+              fit: BoxFit.cover,
+              "https://i.pinimg.com/564x/8c/da/8c/8cda8ca891afa64893369e592fd94822.jpg",
+              //loadingBuilder: (context, ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(Icons.favorite),
+                Text("100"),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(Icons.chat),
+                Text("99"),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
